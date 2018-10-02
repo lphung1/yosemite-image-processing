5f52dc6ef8c07e5bdd03dafe5b9dd713448a3d3d
lphung1 <loi_phung81@yahoo.com>
2018-10-01 22:09:46 -0400

    Implemented the rest of the methods.

--- Yosemite_Image/Image.cpp
+++ Yosemite_Image/Image.cpp
@@ -15,9 +15,6 @@
 using namespace std;
 
 
-
-
-
 class Image{
     
 private:
@@ -31,36 +28,28 @@ private:
     string fileSize; //size of file
     int *image_array;
     int * pic_array = (int *)malloc(1038*807*3*sizeof(int));
-    #define ARR(i,j,k) (pic_array[807*3*i + 3*j + k])
-    
+    #define ARR(i,j,k) (pic_array[1038*3*i + 3*j + k])
+    string fileName;
     
     
 public:
     
-    
-    
-    
     Image(){
         width = height = 0;
-
-        
-        //image_array = image_arr;
         
     }
     
-    //constructor with string file
+    /* constructor with string file, constructor generates new image with file and stores
+    into array
+     */
     Image(string i){
-
         
-        
-        //RGB* image_arr = (RGB*)malloc(1038 * 807);
         pic_array = (int *)malloc(1038*807*3*sizeof(int));
-        
         
         string type, resolution, maxValue;
         
-        string fileName = i;
-        cout << fileName;
+        fileName = i;
+        //cout << fileName;
         
         file.open(fileName);
         if (file.is_open()){
@@ -89,43 +78,107 @@ public:
         
     }//end constructor
     //creates an image object using the given dimensions
-    Image (int width, int height){
+    Image (int w, int h){
         
+        width = w;
+        height = h;
         
     }
-   // destructor  - provide as many destructors as needed
+
     
     // accessors/mutators
-    int getWidth();
-    void setWidth(int w);
-    int getHeight();
-    void setHeight(int h);
+    int getWidth(){
+        return width;
+    }
+    
+    //sets width
+    void setWidth(int w){
+        width = w;
+    }
+    
+    //gets height
+    int getHeight(){
+        return height;
+    }
+    
+    //sets height
+    void setHeight(int h){
+        height = h;
+    }
     
     // set/get an image pixel given row and column addresses
     // pixel is a 3 element r,g,b triple
-    int getImagePixel (int row, int col, int pixel){
+    /*
+     int col - column
+     int row - row
+     int pixel - r, g or b. 0 for r, 1 for g, 2 for b.
+     */
+    int getImagePixel (int col, int row, int pixel){
         
         return ARR(col, row, pixel);
         
     }
     
-    void setImagePixel (int col, int row, int *pixel){
+    /*
+     int col - column
+     int row - row
+     int pixel - r, g or b. 0 for r, 1 for g, 2 for b.
+     int value - new value
+     */
+    int setImagePixel (int col, int row, int pixel, int value){
+        if(value > 255 || value < 0){
+            return 0;
+        }
+        else{
+        ARR(col, row, pixel) = value;
+        return 1;
+        }
         
     }
     
-    // reads an image from  the given input image file
-    void read(string infile);
+    /*
+     reads file and stores in array
+     */
+    void read(string infile){
+        pic_array = (int *)malloc(1038*807*3*sizeof(int));
+        
+        string type, resolution, maxValue;
+        
+        fileName = infile;
+        //cout << fileName;
+        
+        file.open(fileName);
+        if (file.is_open()){
+            cout << "File open\n";
+            getline(file, type); //to skip first 3 lines
+            getline(file, resolution);
+            getline(file, maxValue);
+            //cout << type << "\n" << resolution << "\n" << maxValue << "\n";
+            
+            for(int i = 0; i < 807; i++){
+                for(int j = 0; j < 1038; j ++){
+                    file >> ARR(i,j,red);
+                    file >> ARR(i,j,green);
+                    file >> ARR(i,j,blue);
+                }
+            }
+        }
+        else{
+            cout << "Issue opening file";
+        }
+        file.close();
+    }//end read
     
-    // writes image to file
+    /*
+     displays values of array
+     */
     void display (){
         
         for(int i = 0; i < 1038; i++){
             for(int j = 0; j < 807; j ++){
-                
                 cout << ARR(i,j,red) << " ";
                 cout << ARR(i,j,green) << " ";
                 cout << ARR(i,j,blue) << " ";
-                
             }
             
         }
@@ -137,16 +190,15 @@ public:
         for(int i = 0; i < 807; i++){
             for(int j = 0; j < 1038; j ++){
                 int R, G, B, grey = 0;
-                
+    
                 R = ARR(i,j,red);
                 G = ARR(i,j,green);
                 B = ARR(i,j,blue);
+                
                 grey = (R + G + B) / 3;
                 ARR(i,j,red) = grey;
                 ARR(i,j,green) = grey;
                 ARR(i,j,blue) = grey;
-                
-                
                 
             }
             
@@ -154,18 +206,92 @@ public:
     }
     
     // flips the image  horizontally each row of pixesl
-    void flipHorizontal();
+    void flipHorizontal(){
+        resetImage();
+        
+        string type, resolution, maxValue;
+        file.open(fileName);
+        if (file.is_open()){
+            cout << "File open\n";
+            getline(file, type); //to skip first 3 lines
+            getline(file, resolution);
+            getline(file, maxValue);
+            //cout << type << "\n" << resolution << "\n" << maxValue << "\n";
+            
+            for(int i = 0; i < 807; i++){
+                for(int j = 1038; j > 0; j --){
+                    file >> ARR(i,j,red);
+                    file >> ARR(i,j,green);
+                    file >> ARR(i,j,blue);
+                }
+                
+            }
+            
+        }
+        else{
+            cout << "Issue opening file";
+        }
+        file.close();
+        
+    }
+        
+        
+    
     
     // sets the red component of each pixel to zero
-    void flattenRed();
+    void flattenRed(){
+        resetImage();
+        for(int i = 0; i < 807; i++){
+            for(int j = 0; j < 1038; j ++){
+                ARR(i,j,red) = 0;
+            }
+            
+        }
+    }
     
     // flips the blue component of image about 255
-    void negateBlue();
-    
-    
-    void resetImage(){
+    void negateBlue(){
+        resetImage();
+        for(int i = 0; i < 807; i++){
+            for(int j = 0; j < 1038; j ++){
+                int B = 0;
+                B = ARR(i,j,blue);
+                ARR(i,j,blue) = 255 - B;
+            }
+            
+        }
         
     }
     
+    /*
+     resets modified image to original image that was provided
+     */
+    void resetImage(){
+        
+        string type, resolution, maxValue;
+        
+        file.open(fileName);
+        if (file.is_open()){
+            cout << "File open\n";
+            getline(file, type); //to skip first 3 lines
+            getline(file, resolution);
+            getline(file, maxValue);
+            //cout << type << "\n" << resolution << "\n" << maxValue << "\n";
+            
+            for(int i = 0; i < 807; i++){
+                for(int j = 0; j < 1038; j ++){
+                    file >> ARR(i,j,red);
+                    file >> ARR(i,j,green);
+                    file >> ARR(i,j,blue);
+                }
+                
+            }
+            
+        }
+        else{
+            cout << "Issue opening file";
+        }
+        file.close();
+    }
     
 };
--- Yosemite_Image/main.cpp
+++ Yosemite_Image/main.cpp
@@ -24,35 +24,72 @@ int main() {
     Bridges::setTitle("Greedy Algorithm");
     
     Image yosemite("yosemite.ppm");
-    //yosemite.display();
-
+    
     ColorGrid *cg  = new ColorGrid(807, 1038, Color("white"));
+    
     Bridges::setDataStructure(cg);
     
-    for (int i = 0; i < 1038; i++){
-        for(int j = 0; j < 807; j++){
+    //displays original image
+    for (int i = 0; i < 807; i++){
+        for(int j = 0; j < 1038; j++){
             int red = yosemite.getImagePixel(i, j, 0);
             int green = yosemite.getImagePixel(i, j, 1);
             int blue = yosemite.getImagePixel(i, j, 2);
-            cg -> set(j, i, Color(red, green, blue, 255));
+            cg -> set(i, j, Color(red, green, blue, 255));
         }
     }
-    
     Bridges::visualize();
     
+    //turn image into grayscale and displays
     yosemite.toGrayScale();
     
-    for (int i = 0; i < 1038; i++){
-        for(int j = 0; j < 807; j++){
+    for (int i = 0; i < 807; i++){
+        for(int j = 0; j < 1038; j++){
             int red = yosemite.getImagePixel(i, j, 0);
             int green = yosemite.getImagePixel(i, j, 1);
             int blue = yosemite.getImagePixel(i, j, 2);
-            cg -> set(j, i, Color(red, green, blue, 255));
+            cg -> set(i, j, Color(red, green, blue, 255));
         }
     }
-
+    Bridges::visualize();
     
+    //flatten red values and displays image
+    yosemite.flattenRed();
     
+    for (int i = 0; i < 807; i++){
+        for(int j = 0; j < 1038; j++){
+            int red = yosemite.getImagePixel(i, j, 0);
+            int green = yosemite.getImagePixel(i, j, 1);
+            int blue = yosemite.getImagePixel(i, j, 2);
+            cg -> set(i, j, Color(red, green, blue, 255));
+        }
+    }
+    Bridges::visualize();
+    
+    //negates the blue and displays image
+    yosemite.negateBlue();
+    
+    for (int i = 0; i < 807; i++){
+        for(int j = 0; j < 1038; j++){
+            int red = yosemite.getImagePixel(i, j, 0);
+            int green = yosemite.getImagePixel(i, j, 1);
+            int blue = yosemite.getImagePixel(i, j, 2);
+            cg -> set(i, j, Color(red, green, blue, 255));
+        }
+    }
+    Bridges::visualize();
+    
+    //flips image and display image
+    yosemite.flipHorizontal();
+    
+    for (int i = 0; i < 807; i++){
+        for(int j = 0; j < 1038; j++){
+            int red = yosemite.getImagePixel(i, j, 0);
+            int green = yosemite.getImagePixel(i, j, 1);
+            int blue = yosemite.getImagePixel(i, j, 2);
+            cg -> set(i, j, Color(red, green, blue, 255));
+        }
+    }
     Bridges::visualize();
     
     
